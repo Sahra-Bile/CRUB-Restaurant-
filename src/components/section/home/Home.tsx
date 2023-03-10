8e93a812e2aaec99868da329ffb333e9ab9ff200
Tove Bosom <bboson@live.se>
2023-03-10 15:28:11 +0100

    Added about to homepage

--- src/components/section/home/Home.tsx
+++ src/components/section/home/Home.tsx
@@ -4,25 +4,28 @@ import { About } from "../about/About";
 
 export const Home = () => {
   return (
-    <section className="home" id="home">
-      <div className="home__container">
-        <h1 className="home__container__header">CRUB.</h1>
-        <p className="home__container__info">
-          Vi erbjuder svensk husmanskost på vegetariskt vis.
-        </p>
-      </div>
-      <div
-        className="home
+    <>
+      <section className="home" id="home">
+        <div className="home__container">
+          <h1 className="home__container__header">CRUB.</h1>
+          <p className="home__container__info">
+            Vi erbjuder svensk husmanskost på vegetariskt vis.
+          </p>
+        </div>
+        <div
+          className="home
         <About />
       __presentationContainer"
-      >
-        <p className="home__presentationContainer__info">
-          Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
-          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrum exercitationem ullam corporis suscipit
-          laboriosam, nisi ut aliquid ex ea commodi consequatur.
-        </p>
-      </div>
-    </section>
+        >
+          <p className="home__presentationContainer__info">
+            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+            tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrum exercitationem ullam corporis suscipit
+            laboriosam, nisi ut aliquid ex ea commodi consequatur.
+          </p>
+        </div>
+      </section>
+      <About />
+    </>
   );
 };
