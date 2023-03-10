e7e9c10beddc6023330c3574438ac8302a7d769e
Tove Bosom <bboson@live.se>
2023-03-10 15:11:04 +0100

    Home and footersection done

--- src/components/section/footer/Footer.tsx
+++ src/components/section/footer/Footer.tsx
@@ -1,5 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./footer.scss";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export const Footer = () => {
-  return <div>Footer</div>
-}
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <ul className="footer__container__sectioning">
+          <li className="footer__container__list">Besök oss på:</li>
+          <li className="footer__container__list">Drottninggatan 33 </li>
+          <li className="footer__container__list">156 78 Stockholm</li>
+        </ul>
+        <ul className="footer__container__sectioning">
+          <li className="footer__container__list">Telefon: 076-123 45 67</li>
+          <li className="footer__container__list mail">
+            Mail: kontakt@crub.se
+          </li>
+        </ul>
+        <ul className="footer__container__column">
+          <li className="footer__container__column__icon">
+            <FaFacebook />
+          </li>
+          <li className="footer__container__column__icon">
+            <FaLinkedin />
+          </li>
+          <li className="footer__container__column__icon">
+            <FaInstagram />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
--- src/components/section/home/Home.tsx
+++ src/components/section/home/Home.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./home.scss";
 
 export const Home = () => {
   return (
-    <section id="contact">
-      <div>Home</div>
+    <section className="home" id="home">
+      <div className="home__container">
+        <h1 className="home__container__header">CRUB.</h1>
+        <p className="home__container__info">
+          Vi erbjuder svensk husmanskost på vegetariskt vis.
+        </p>
+      </div>
+      <div className="home__presentationContainer">
+        <p className="home__presentationContainer__info">
+          Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrum exercitationem ullam corporis suscipit
+          laboriosam, nisi ut aliquid ex ea commodi consequatur.
+        </p>
+      </div>
     </section>
-  )
-}
+  );
+};
