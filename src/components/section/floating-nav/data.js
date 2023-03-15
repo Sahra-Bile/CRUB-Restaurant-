184c32de97904bca98c9a83de94c5faed397eb94
henrietta jeansson <henriettajeansson@gmail.com>
2023-03-15 16:23:17 +0100

    buttons in admin page

--- src/components/section/floating-nav/data.js
+++ src/components/section/floating-nav/data.js
@@ -1,18 +1,20 @@
-import {AiFillHome} from 'react-icons/ai'
+/** @format */
+
+import { AiFillHome } from "react-icons/ai";
 // import {BsPersonFill} from 'react-icons/bs'
-import {MdOutlineRestaurantMenu} from 'react-icons/md'
-import {AiFillMessage} from 'react-icons/ai'
-import {GiTabletopPlayers} from 'react-icons/gi'
-import{RiAdminLine} from 'react-icons/ri'
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import { AiFillMessage } from "react-icons/ai";
+import { RiAdminLine } from "react-icons/ri";
 
+import { AiOutlineCalendar } from "react-icons/ai";
 
- export const data = [
-    {id: 1, link: '/', icon: <AiFillHome/>},
-    // {id: 2, link: '/about', icon: <BsPersonFill/>},
-    {id: 3, link: '/booking', icon: <GiTabletopPlayers/>},
-    {id: 4, link: '/menu', icon: <MdOutlineRestaurantMenu/>},
-    {id: 5, link: '/contact', icon: <AiFillMessage/>},
-    {id: 6, link: '/admin', icon: <RiAdminLine/>}
-]
+import { MdPermPhoneMsg } from "react-icons/md";
 
-
+export const data = [
+  { id: 1, link: "/", icon: <AiFillHome /> },
+  // {id: 2, link: '/about', icon: <BsPersonFill/>},
+  { id: 3, link: "/booking", icon: <AiOutlineCalendar /> },
+  { id: 4, link: "/menu", icon: <MdOutlineRestaurantMenu /> },
+  { id: 5, link: "/contact", icon: <MdPermPhoneMsg /> },
+  { id: 6, link: "/admin", icon: <RiAdminLine /> },
+];
